ee863b61df5d7814a8ccc61e6390184a1d1b42ea
golden2402 <golden2402@vt.edu>
2023-10-14 19:42:59 -0400

    scaffold Docker structure

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
 # animate
+
+## Setup
+
+### Omitted Files
+
+For production environments, the compose files (both development and production) require the existence of a few secrets/misc. files omitted from version control:
+
+- `client/.env`
+- `server/.env`
+
+- `pg/.env`&mdash;generic database credentials, sample:
+    ```properties
+    PG_USER=postgres
+    POSTGRES_DB=animate
+    # only used for development--replaced with a secret in production:
+    POSTGRES_PASSWORD=postgres
+    ```
+- `.secrets/db/password`&mdash;(production) database admin password
+
+## Startup
+
+```bash
+# -d flag runs containers in detached mode:
+docker-compose up [-d]
+# or, for development mode:
+docker-compose -f compose.dev.yml up [-d]
+```
+
+For first, invocation when switching between invocation files (e.g. default `compose.yml` to `compose.dev.yml`), append the `--build` flag to invoke a full container rebuild.
+
+## Teardown
+
+```bash
+docker-compose down
+```
+
+If you want to remove containers *and* their associated volumes, append the `-v` flag. **Purging volumes will result in the removal of all Postgres data.**
---
+++ pg/sql/init/create_tables.sql
@@ -0,0 +1,126 @@
+create domain hex_color char(6) default '1e293b' not null;
+
+create table if not exists
+  user_account (
+    -- identification:
+    id serial primary key,
+    email varchar(80) unique,
+    username varchar(20) unique,
+    user_password varchar(255),
+    -- user presentation:
+    display_name varchar(80),
+    user_color hex_color,
+    blurb varchar(255)
+  );
+
+create table if not exists
+  follow (
+    followee_id serial primary key,
+    foreign key (followee_id) references user_account (id) on delete cascade,
+    follower_id serial,
+    foreign key (follower_id) references user_account (id) on delete cascade
+  );
+
+create domain date_year as smallint check (value > 1799);
+
+create type season_type as enum('winter', 'spring', 'fall', 'summer');
+
+create table if not exists
+  season (
+    id serial primary key,
+    season_year date_year not null,
+    season_name season_type not null
+  );
+
+create table if not exists
+  anime (
+    id serial primary key,
+    title varchar(255),
+    season_id serial,
+    foreign key (season_id) references season (id) on delete set null
+  );
+
+create table if not exists
+  favorite (
+    user_id serial,
+    foreign key (user_id) references user_account (id),
+    anime_id serial,
+    foreign key (anime_id) references anime (id)
+  );
+
+create table if not exists
+  genre (
+    id serial primary key,
+    genre_name varchar(80) unique
+  );
+
+create table if not exists
+  anime_genre (
+    anime_id serial,
+    foreign key (anime_id) references anime (id),
+    genre_id serial,
+    foreign key (genre_id) references genre (id)
+  );
+
+create table if not exists
+  episode (
+    id serial primary key,
+    episode_number smallint not null,
+    episode_name varchar(80)
+  );
+
+create table if not exists
+  anime_episode (
+    anime_id serial,
+    foreign key (anime_id) references anime (id),
+    episode_id serial,
+    foreign key (episode_id) references episode (id)
+  );
+
+create table if not exists
+  watched_episodes (
+    user_id serial,
+    foreign key (user_id) references user_account (id),
+    episode_id serial,
+    foreign key (episode_id) references episode (id),
+    watch_date date
+  );
+
+-- FIXME: rating and review might need some revision; how should we couple the 
+-- two (if at all?):
+create table if not exists
+  rating (
+    user_id serial,
+    foreign key (user_id) references user_account (id),
+    episode_id serial,
+    foreign key (episode_id) references episode (id),
+    -- should this be not null?:
+    rate_score smallint not null check (
+      rate_score >= 0
+      and rate_score <= 10
+    ),
+    rate_date date
+  );
+
+create table if not exists
+  review (
+    user_id serial,
+    foreign key (user_id) references user_account (id),
+    post varchar(2047) not null
+  );
+
+create table if not exists
+  producer (
+    id serial primary key,
+    studio_name varchar(80),
+    studio_year date_year,
+    studio_blurb varchar(255)
+  );
+
+create table if not exists
+  produced_by (
+    anime_id serial,
+    foreign key (anime_id) references anime (id),
+    producer_id serial,
+    foreign key (producer_id) references producer (id)
+  );
