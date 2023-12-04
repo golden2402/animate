bd8e0fc53c8f9fdb63ae70a4f87c24faede93573
golden2402 <golden2402@vt.edu>
2023-12-04 01:48:02 -0500

    edit card presentation

--- client/src/app/(sidebar)/(contained)/anime/page.tsx
+++ client/src/app/(sidebar)/(contained)/anime/page.tsx
@@ -1,7 +1,17 @@
 "use client";
 
-import { useEffect, useState } from "react"
-import { AnimeItemModel, SeasonItemResponseModel, WatchItemResponseModel } from "@/types/data-models"
+import Link from "next/link";
+import { useEffect, useState } from "react";
+
+import {
+  AnimeItemModel,
+  SeasonItemResponseModel,
+  WatchItemResponseModel
+} from "@/types/data-models";
+
+import SearchIcon from "@/components/icons/SearchIcon";
+
+import joinClasses from "@/util/join-classes";
 import fetchWithToken from "@/util/api/fetch-with-token";
 
 export default function DisplayAnime() {
@@ -43,106 +53,143 @@ export default function DisplayAnime() {
     async function getAnimeBySeason(id: number | undefined) {
       const response = await fetch(`/api/anime/seasons/${id}`);
 
-            if (response.ok) {
-                setAnimes(await response.json());
-            }
-        }
-        getAnimeBySeason(temp[0].id)
+      if (response.ok) {
+        setAnimes(await response.json());
+      }
+    }
+    getAnimeBySeason(temp[0].id);
+  }
+
+  function addAnimeToWatchList(anime: AnimeItemModel) {
+    async function watchAnime(watch: WatchItemResponseModel) {
+      const response = await fetchWithToken(`/api/anime/watch`, {
+        headers: {
+          "content-type": "application/json"
+        },
+        method: "POST",
+        body: JSON.stringify(watch)
+      });
+
+      if (response.ok) {
+        const tempAnime = animes?.filter((anime2) => {
+          if (anime.id == anime2.id) {
+            anime.watched = true;
+          }
+          return anime2;
+        });
+        console.log(tempAnime);
+        setAnimes(tempAnime);
+      }
     }
 
-    function addAnimeToWatchList(anime: AnimeItemModel) {
-        
-        async function watchAnime(watch: WatchItemResponseModel) {
-            const response = await fetchWithToken(`/api/anime/watch`, {
-                headers: {
-                    "content-type": "application/json"
-                  },
-                method: "POST",
-                body: JSON.stringify(watch)
-            });
-
-            if (response.ok) {
-                const tempAnime = animes?.filter(anime2 => {
-                    if (anime.id == anime2.id) {
-                        anime.watched = true
-                    }
-                    return anime2
-                })
-                console.log(tempAnime)
-                setAnimes(tempAnime)
-            }
+    async function updateWatch(watch: WatchItemResponseModel) {
+      const response = await fetchWithToken(`/api/anime/watch`, {
+        headers: {
+          "content-type": "application/json"
+        },
+        method: "DELETE",
+        body: JSON.stringify(watch)
+      });
+      const tempAnime = animes?.filter((anime2) => {
+        if (anime.id == anime2.id) {
+          anime.watched = false;
         }
-
-
-        async function updateWatch(watch: WatchItemResponseModel) {
-            const response = await fetchWithToken(`/api/anime/watch`, {
-                headers: {
-                    "content-type": "application/json"
-                  },
-                method: "DELETE",
-                body: JSON.stringify(watch)
-            });
-            const tempAnime = animes?.filter(anime2 => {
-                if (anime.id == anime2.id) {
-                    anime.watched = false
-                }
-                return anime2
-            })
-            setAnimes(tempAnime)
-        }
-
-        const watch: WatchItemResponseModel = {
-            anime_id: anime.id,
-            watch_date: new Date().toISOString()
-        }
-        if (anime.watched) {
-            updateWatch(watch)
-        } else {
-            watchAnime(watch)
-        }
+        return anime2;
+      });
+      setAnimes(tempAnime);
     }
+
+    const watch: WatchItemResponseModel = {
+      anime_id: anime.id,
+      watch_date: new Date().toISOString()
+    };
+    if (anime.watched) {
+      updateWatch(watch);
+    } else {
+      watchAnime(watch);
+    }
+  }
 
   return (
     <div>
-      <div className="flex flex-row mx-5">
-        <input
-          onChange={(e) =>
-            setCurrent({
-              ...current,
-              season_name: e.target.value
-            })
-          }
-          value={current?.season_name}
-          placeholder="Season Name"
-        ></input>
+      <div className="flex flex-row gap-2">
+        <div
+          className="
+          flex justify-between gap-1 items-center
+          w-full
+          p-1
+          rounded
+          outline outline-1 outline-black/20"
+        >
+          <div className="flex w-full">
+            <input
+              className="bg-transparent w-full h-6 px-1 text-sm"
+              onChange={(e) =>
+                setCurrent({
+                  ...current,
+                  season_name: e.target.value
+                })
+              }
+              value={current?.season_name}
+              placeholder="Season Name"
+            />
 
-        <input
-          onChange={(e) =>
-            setCurrent({
-              ...current,
-              season_year: e.target.value
-            })
-          }
-          value={current?.season_year}
-          type="number"
-          placeholder="Season Year"
-        ></input>
-
-                <button onClick={() => getAnimeBySeason()}>Search</button>
-            </div>
-            <div className="flex flex-wrap gap-2 mt-8">
-                {animes?.slice(0, 500).map((anime, i) => {
-                    return (
-                        <div key={i} className="items-center flex-col outline-2 bg-neutral-300">
-                            <h1 className="text-lg font-bold">{anime.title}</h1>
-                            <p className="text-sm font-medium">Episode Count: {anime.episodes ? anime.episodes : "N/A"}</p>
-                            <p>{anime.blurb}</p>
-                            <button onClick={() => addAnimeToWatchList(anime)}>{anime.watched ? "Remove From Watched List" : "Add to Watch List"}</button>
-                        </div>
-                    )
-                })}
-            </div>
+            <input
+              className="bg-transparent w-full h-6 px-1 text-sm"
+              onChange={(e) =>
+                setCurrent({
+                  ...current,
+                  season_year: e.target.value
+                })
+              }
+              value={current?.season_year}
+              type="number"
+              placeholder="Season Year"
+            />
+          </div>
         </div>
-    )
 
+        <button className="flex items-center gap-1 py-1 px-2 rounded bg-blue-500 text-neutral-100 outline outline-1 outline-black/20 text-sm font-medium" onClick={() => getAnimeBySeason()}>
+          <p>Search</p>
+          <SearchIcon className="w-5" />
+        </button>
+      </div>
+
+      <div className="flex flex-col gap-2 mt-8">
+        {animes?.slice(0, 500).map((anime, i) => {
+          return (
+            <div
+              key={i}
+              className="flex flex-col gap-2 p-6 rounded fg fg-outline fg-shadow"
+            >
+              <div>
+                <Link href={`/anime/${anime.id}`}>
+                  <h1 className="text-xl font-semibold underline">{anime.title}</h1>
+                </Link>
+                <p className="text-sm font-medium">
+                  Episodes: {anime.episodes ? anime.episodes : "N/A"}
+                </p>
+              </div>
+
+              {anime.blurb && (
+                <p className="text-sm p-2 rounded fg fg-outline">
+                  {anime.blurb}
+                </p>
+              )}
+
+              <button
+                className={joinClasses(
+                  "p-1 rounded fg-outline text-neutral-50",
+                  anime.watched ? "bg-red-500" : "bg-green-500"
+                )}
+                onClick={() => addAnimeToWatchList(anime)}
+              >
+                {anime.watched ? "Remove from Watchlist" : "Add to Watchlist"}
+              </button>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
